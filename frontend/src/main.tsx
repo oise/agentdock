ba38b4ed838f7947ccc616c57004bcbfae9c37ac
oise <oise@Carbide.local>
2026-06-15 22:48:13 +0200

    fix: remove scroll acceleration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { installJcefHostRepaintCoordinator } from './utils/jcefHostRepaint.ts'
-import { ACPBridge } from './utils/bridge.ts'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { installJcefHostRepaintCoordinator } from './utils/jcefHostRepaint.ts';
+import { ACPBridge } from './utils/bridge.ts';
 
-installJcefHostRepaintCoordinator()
-ACPBridge.initialize()
+installJcefHostRepaintCoordinator();
+//installJcefScrollFix()
+ACPBridge.initialize();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
